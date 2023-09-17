f145042b74eba8841ea1d7f003a1297184f26987
gourav <gouravbksc1@gmai.com>
2023-09-17 12:33:25 +0530

    feat: listing reservation page

--- src/app/listing/[listingId]/listing-client.js
+++ src/app/listing/[listingId]/listing-client.js
@@ -2,6 +2,7 @@
 import Container from '@/components/container';
 import { useMemo } from 'react';
 import ListingHead from './listing-head';
+import ListingInfo from './listing-info';
 
 const { categories } = require('@/components/navbar/categories');
 
@@ -20,6 +21,24 @@ const ListingClient = ({ listing, currentUser, reservations }) => {
             id={listing.id}
             currentUser={currentUser}
           />
+          <div
+            className="grid
+          grid-cols-1
+          md:grid-cols-7
+          md:gap-10
+          mt-6
+          "
+          >
+            <ListingInfo
+              user={listing.user}
+              category={category}
+              description={listing.description}
+              roomCount={listing.roomCount}
+              guestCount={listing.guestCount}
+              bathroomCount={listing.bathroomCount}
+              locationValue={listing.locationValue}
+            />
+          </div>
         </div>
       </div>
     </Container>
--- src/app/listing/[listingId]/listing-head.js
+++ src/app/listing/[listingId]/listing-head.js
@@ -1,6 +1,43 @@
 'use client';
+
+import Heading from '@/components/heading';
+import HeartButton from '@/components/heart-button';
+import useCountries from '@/hooks/useCountries';
+import Image from 'next/image';
+
 const ListingHead = ({ title, imageSrc, locationValue, id, currentUser }) => {
-  return <div className="flex flex-col gap-6">jksjdksk</div>;
+  const { getByValue } = useCountries();
+
+  const location = getByValue(locationValue);
+  return (
+    <>
+      <Heading
+        title={title}
+        subtitle={`
+          ${location.region}, ${location.label}
+      `}
+      />
+      <div
+        className="
+        relative 
+        h-[60vh]
+        overflow-hidden
+        rounded-xl
+        relative
+        "
+      >
+        <Image
+          src={imageSrc}
+          alt="Image of the listing"
+          fill
+          className="object-cover w-full"
+        />
+        <div className="absolute top-5 right-5">
+          <HeartButton listingId={id} currentUser={currentUser} />
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default ListingHead;
